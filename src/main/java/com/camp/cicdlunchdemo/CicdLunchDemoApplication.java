963bb7361aa2220519efb432a0368e13d618e423
camp-robbie <camp.robbie.choi@gmail.com>
2026-01-22 00:46:40 +0900

    init commit

---
+++ src/main/java/com/camp/cicdlunchdemo/CicdLunchDemoApplication.java
@@ -0,0 +1,13 @@
+package com.camp.cicdlunchdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CicdLunchDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdLunchDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/camp/cicdlunchdemo/CicdLunchDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.camp.cicdlunchdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdLunchDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
